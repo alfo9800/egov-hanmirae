d7909a4e3a581a760773ee57ba15185bde94def3
user <user@DESKTOP-SI1MC9N>
2021-02-10 15:48:18 +0900

    egov 커스터마이징 시작 준비2

--- src/main/java/edu/human/com/common/EgovComAbstractMapper.java
+++ src/main/java/edu/human/com/common/EgovComAbstractMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,13 +34,17 @@ public abstract class EgovComAbstractMapper extends EgovAbstractMapper {
 		super.setSqlSessionFactory(sqlSession);
 	}
 	
-	//오버라이드
+	//오버라이드	
 	@Override
-	public int delete(String queryId, Object parameterObject) {
-		// TODO Auto-generated method stub
-		return super.delete(queryId, parameterObject);
+	public int delete(String queryId) {
+		return getSqlSession().delete(queryId);
 	}
 	
+	@Override
+	public int delete(String queryId, Object parameterObject) {
+		return getSqlSession().delete(queryId, parameterObject);
+	}
+
 	@Override
 	public int insert(String queryId) {
 		return getSqlSession().insert(queryId);
@@ -52,20 +57,23 @@ public abstract class EgovComAbstractMapper extends EgovAbstractMapper {
 	
 	@Override
 	public <E> List<E> selectList(String queryId, Object parameterObject) {
-		// TODO Auto-generated method stub
-		return super.selectList(queryId, parameterObject);
+		return getSqlSession().selectList(queryId, parameterObject);
 	}
 
 	@Override
 	public <E> List<E> selectList(String queryId) {
-		// TODO Auto-generated method stub
-		return super.selectList(queryId);
+		return getSqlSession().selectList(queryId);
 	}
 	
+	
+	@Override
+	public <T> T selectOne(String queryId) {
+		return getSqlSession().selectOne(queryId);
+	}
+
 	@Override
 	public <T> T selectOne(String queryId, Object parameterObject) {
-		// TODO Auto-generated method stub
-		return super.selectOne(queryId, parameterObject);
+		return getSqlSession().selectOne(queryId, parameterObject);
 	}
 	
 	@Override
@@ -76,6 +84,25 @@ public abstract class EgovComAbstractMapper extends EgovAbstractMapper {
 	@Override
 	public int update(String queryId, Object parameterObject) {
 		return getSqlSession().update(queryId, parameterObject);
+	}
+
+	
+	@Override
+	public <E> List<E> selectList(String queryId, Object parameterObject, RowBounds rowBounds) {
+		return getSqlSession().selectList(queryId, parameterObject, rowBounds);
+	}
+
+	//========================================================================================
+	/* 페이징범위 계산: pageIndex(선택한페이지) 외 pageSize(1페이지당 보여줄 개수) 2개 값을 매개변수로 받아서
+	 * 1페이지 일 때 시작*쿼리번호 : 10[1페이지당 출력할 개수]x(1[선택한 페이지번호]-1) = 0
+	 * 2페이지 일 때 시작*쿼리번호 : 10x(2-1) = 10[계산결과나온 시작페이지 번호] = 10
+	 * offset = (pageIndex-1)*pageSize; 
+	 */
+	@Override
+	public List<?> listWithPaging(String queryId, Object parameterObject, int pageIndex, int pageSize) {
+		int offset = (pageIndex-1) * pageSize;
+		RowBounds rowBounds = new RowBounds(offset, pageSize); //(시작인덱스 번호,꺼내올 개수)
+		return getSqlSession().selectList(queryId, parameterObject, rowBounds);
 	}
 	
 	
